09b9ad2dae993e09ea52611538b2a887e4c5d07b
mubashirnhasir <mubashirnhasir@gmail.com>
2025-03-18 00:59:29 +0530

    just a small change

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-console.log("This is my first backend");
-console.log("This okkaay ");
+// console.log("This is my first backend");
+// console.log("This okkaay ");
 
-// process.exit(1)
-process.exit=1
+// // process.exit(1)
+// process.exit=1
+
+const _ = require("lodash")
+const arr = [1,2,3,4,5]
+console.log(_.last(arr));
