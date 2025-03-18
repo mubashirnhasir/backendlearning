d0f4293382b464dede222e1b5022e73269f7a765
mubashirnhasir <mubashirnhasir@gmail.com>
2025-03-19 00:15:16 +0530

    Callbacks, promises, async await

---
+++ async.js
@@ -0,0 +1,14 @@
+console.log("Mubashir")
+
+
+async function sleep(milliseconds){
+    
+    console.log("Operation is running");
+    setTimeout(()=>{
+        console.log("Operation is done")
+    }, milliseconds)
+}
+
+sleep(1000)
+
+console.log("HEHEHHEHEH");
--- index.js
+++ index.js
@@ -15,7 +15,55 @@
 //     t:"WW"
 // }))
 
-const {tesla,hyndai} = require("./car")
+// const {tesla,hyndai} = require("./car")
 
-console.log(JSON.stringify(tesla, null, 2))
-console.log(JSON.stringify(hyndai, null, 2))
+// console.log(JSON.stringify(tesla, null, 2))
+// console.log(JSON.stringify(hyndai, null, 2))
+
+const path = require("path")
+
+const file = "C:/Users/talen/OneDrive/Desktop/backendlearning/files/sample.txt"
+
+// // nam of the file
+// console.log(path.basename(file));
+
+// //name of the extention
+// console.log(path.extname(file));
+
+// //name of the directory
+// console.log(path.dirname(file));
+
+const fs = require("fs")
+const fsPromise = require("fs/promises")
+// fs.readFile(file, "utf-8", (err,data )=>{
+//     if(err) throw new Error("Not a file you are looking for ")
+//         console.log(data);
+
+// })
+
+const textFile = path.join(__dirname, "files", "text.txt")
+const content = "I love fasting during ramadan"
+
+// fs.writeFile(textFile, content, (error) => {
+//     if (error) throw new Error("Something went wrong")
+//     console.log("Write operation completed Succesfylly");
+//     fs.readFile(textFile, "utf-8", (error, data) => {
+//         if (error) throw new Error("Unable to read file from the system")
+//         console.log(data);
+
+//     })
+// })
+
+const writingData = async () => {
+    try {
+        await fsPromise.writeFile(textFile, content)
+        await fsPromise.appendFile(textFile, "\n This is an appending file data")
+        const data = await fsPromise.readFile(textFile, "utf-8")
+        console.log(data);
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+writingData()
